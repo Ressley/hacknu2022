9e04c34c9e9386e367c2aeaac272c29645578f83
Temirlan <temirlan@pop-os.localdomain>
2022-09-25 03:39:58 +0600

    Longitude Latitude to float64

--- internal/app/apiserver/models/building.go
+++ internal/app/apiserver/models/building.go
@@ -9,8 +9,8 @@ import (
 type Building struct {
 	ID           primitive.ObjectID `bson:"_id, omitempty"`
 	Name         *string            `json:"name" validate:"required,min=2,max=100"`
-	Latitude     *string            `json:"latitude" validate:"required"`
-	Longitude    *string            `json:"longitude" validate:"required"`
+	Latitude     *float64           `json:"latitude" validate:"required"`
+	Longitude    *float64           `json:"longitude" validate:"required"`
 	Started_at   time.Time          `json:"started_at" validate:"required"`
 	Ends_at      time.Time          `json:"ends_at" validate:"required"`
 	Floors       *int               `json:"floors" validate:"required"`
@@ -22,8 +22,8 @@ type Building struct {
 type BuildingResponse struct {
 	ID           primitive.ObjectID    `json:"id"`
 	Name         *string               `json:"name"`
-	Latitude     *string               `json:"latitude"`
-	Longitude    *string               `json:"longitude"`
+	Latitude     *float64              `json:"latitude"`
+	Longitude    *float64              `json:"longitude"`
 	Started_at   time.Time             `json:"started_at"`
 	Ends_at      time.Time             `json:"ends_at"`
 	Floors       *int                  `json:"floors"`
